8d0fc96d0101747b240100e9168295647e11283c
Fatai123 <fataioyetunji34@gmail.com>
2022-09-16 12:06:54 +0100

    return current square area

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+class Square:
+    """ To define a square
+        Private instance attribute: size
+        Instantiation with size (no type/value verification)
+    """
+    def __init__(self, size=0):
+        """Initialize the size and verify the type and value"""
+        if not isinstance(size, int):
+            raise TypeError ("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """Return the current square area"""
+        return self.__size * self.__size
